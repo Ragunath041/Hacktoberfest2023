4c08b7c7ed6378b685f1ccbe0325954fab3d79da
RAGUNATH G <114632312+Ragunath041@users.noreply.github.com>
2023-10-01 16:42:46 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2050,9 +2050,10 @@ contributors = [
     id:422,
     fullname:"Evgeny Skorlov",
     username:"https://github.com/ru-asdx",
+  },
+  {
+    id : 423,
+    fullname : "Ragunath G",
+    username : "https://github.com/Ragunath041",
   }
-
-
-
-
 ];
